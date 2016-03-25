cbe30009172704ae2a4fc67e752cfc5c0a2ab0e0
Grant Palin <grant@grantpalin.com>
2016-03-24 23:16:34 -0700

    Initial commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+require_once get_stylesheet_directory() . '/inc/tgm.php';
+
+function twenty_fifteen_cmb2_enqueue() {
+	$parent_style = 'parent-style'; // using a variable to ensure that parent theme style loads first
+
+	// load the parent theme styles
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
+	// load the child theme styles
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ), // require parent theme style to be loaded first
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'twenty_fifteen_cmb2_enqueue' );
